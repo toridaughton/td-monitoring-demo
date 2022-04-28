6bcfb2f4ed5306c39160a49175d58035217f656a
Tori Daughton <toriddaughton@gmail.com>
2022-04-28 12:38:57 -0600

    second attempt rollbar

--- server/index.js
+++ server/index.js
@@ -6,14 +6,13 @@ const app = express()
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
-  accessToken: 'fcd6fd019ed846d3b2794dbe9d731107',
+  accessToken: '38c605cdba524ad2ac7636e72662c0e3',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
 
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
